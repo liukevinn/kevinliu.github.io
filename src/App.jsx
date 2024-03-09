d7a50b1e97e2917b40961d6673a5bbe217af5267
Kevin Liu <kevinwliuu@gmail.com>
2024-03-09 16:56:07 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = () => {
+
+
+  return (
+        <div>
+          <p>Welcome to my Portfolio!</p>
+        <img src="/src/images/kevinliuPhoto.png" alt = "face photo"/>
+        </div>
+  )
+}
+
+export default App
